18c1b0fadb5828c7d6ca4d5f9ed7ec443b14fae9
Mark Lavercombe <mlava@users.noreply.github.com>
2014-03-09 22:45:07 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var Device = require('./lib/device')
   , stream = require('stream')
   , configHandlers = require('./lib/config-handlers');
 
-// Give our driver a stream interface
-util.inherits(myDriver,stream);
+util.inherits(driver,stream);
 
-// Our greeting to the user.
 var HELLO_WORLD_ANNOUNCEMENT = {
   "contents": [
     { "type": "heading",      "text": "Autoremote Driver Loaded" },
@@ -14,57 +12,28 @@ var HELLO_WORLD_ANNOUNCEMENT = {
   ]
 };
 
-/**
- * Called when our client starts up
- * @constructor
- *
- * @param  {Object} opts Saved/default driver configuration
- * @param  {Object} app  The app event emitter
- * @param  {String} app.id The client serial number
- *
- * @property  {Function} save When called will save the contents of `opts`
- * @property  {Function} config Will be called when config data is received from the Ninja Platform
- *
- * @fires register - Emit this when you wish to register a device (see Device)
- * @fires config - Emit this when you wish to send config data back to the Ninja Platform
- */
-function myDriver(opts,app) {
+
+function driver(opts,app) {
 
   var self = this;
 
   app.on('client::up',function(){
 
-    // The client is now connected to the Ninja Platform
-
-    // Check if we have sent an announcement before.
-    // If not, send one and save the fact that we have.
     if (!opts.hasSentAnnouncement) {
       self.emit('announcement',HELLO_WORLD_ANNOUNCEMENT);
       opts.hasSentAnnouncement = true;
       self.save();
     }
 
-    // Register a device
     self.emit('register', new Device());
   });
 };
 
-/**
- * Called when a user prompts a configuration.
- * If `rpc` is null, the user is asking for a menu of actions
- * This menu should have rpc_methods attached to them
- *
- * @param  {Object}   rpc     RPC Object
- * @param  {String}   rpc.method The method from the last payload
- * @param  {Object}   rpc.params Any input data the user provided
- * @param  {Function} cb      Used to match up requests.
- */
-myDriver.prototype.config = function(rpc,cb) {
+
+driver.prototype.config = function(rpc,cb) {
 
   var self = this;
-  // If rpc is null, we should send the user a menu of what he/she
-  // can do.
-  // Otherwise, we will try action the rpc method
+
   if (!rpc) {
     return configHandlers.menu.call(this,cb);
   }
@@ -79,4 +48,4 @@ myDriver.prototype.config = function(rpc,cb) {
 
 
 // Export it
-module.exports = myDriver;
+module.exports = driver;
